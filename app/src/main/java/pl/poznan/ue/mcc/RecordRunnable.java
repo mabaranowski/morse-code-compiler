5521cd4a05bd68b2ca6d5184943fdd979b56170d
kr1zr <kr1zr@users.noreply.github.com>
2019-11-22 11:05:47 +0100

    Thread sync and real-time preview.
    Added thread synchronization and real-time preview.

--- app/src/main/java/pl/poznan/ue/mcc/RecordRunnable.java
+++ app/src/main/java/pl/poznan/ue/mcc/RecordRunnable.java
@@ -19,6 +19,7 @@ public class RecordRunnable implements Runnable {
     private Boolean isSignal;
     private Boolean isLetter;
     private Boolean isWord;
+    private Boolean isRunning;
 
     public RecordRunnable(MediaRecorder recorder) {
         this.recorder = recorder;
@@ -26,19 +27,23 @@ public class RecordRunnable implements Runnable {
         this.isSignal = false;
         this.isLetter = false;
         this.isWord = false;
+        this.isRunning = false;
     }
 
     public void run() {
         StringBuilder sb = new StringBuilder();
+        Converter converter = new Converter();
         int spaceCounter = 0;
+        isRunning = true;
 
-        while(shouldRun) {
-            if(recorder == null) {
+        while (shouldRun) {
+            if (recorder == null) {
+                isRunning = false;
                 break;
             }
 
             int amp = recorder.getMaxAmplitude();
-            if(amp > 500 && amp < 2000) {
+            if (amp > 500 && amp < 2000) {
                 appendSeparators(sb);
                 spaceCounter = 0;
 
@@ -52,16 +57,18 @@ public class RecordRunnable implements Runnable {
                 Log.e(LOG_TAG, amp + "");
             } else {
                 spaceCounter++;
-                if(spaceCounter > 3 && spaceCounter < 9) {
+                if (spaceCounter > 3 && spaceCounter < 9) {
                     isSignal = true;
-                } else if(spaceCounter >= 9 && spaceCounter < 21) {
+                } else if (spaceCounter >= 9 && spaceCounter < 21) {
                     isSignal = false;
                     isLetter = true;
-                } else if(spaceCounter >= 21) {
+                } else if (spaceCounter >= 21) {
                     isLetter = false;
                     isWord = true;
                 }
             }
+
+            MainActivity.textArea.setText(converter.convert(sb.toString()));
 
             try {
                 Thread.sleep(100);
@@ -70,10 +77,15 @@ public class RecordRunnable implements Runnable {
             }
         }
 
-        if(!shouldRun) {
+        if (!shouldRun) {
             String stringFromBuilder = sb.toString();
             this.quinaryExpression = stringFromBuilder;
             Log.i(LOG_TAG, stringFromBuilder);
+
+            isRunning = false;
+            synchronized (Recorder.LOCK) {
+                Recorder.LOCK.notifyAll();
+            }
         }
     }
 
@@ -81,18 +93,22 @@ public class RecordRunnable implements Runnable {
         this.shouldRun = shouldRun;
     }
 
+    public Boolean isRunning() {
+        return isRunning;
+    }
+
     public String getQuinaryExpression() {
         return quinaryExpression;
     }
 
     private void appendSeparators(StringBuilder sb) {
-        if(isSignal) {
+        if (isSignal) {
             sb.append(SIGNAL_SEP);
             Log.i(LOG_TAG, "SIGNAL");
-        } else if(isLetter) {
+        } else if (isLetter) {
             sb.append(LETTER_SEP);
             Log.i(LOG_TAG, "LETTER");
-        } else if(isWord) {
+        } else if (isWord) {
             sb.append(WORD_SEP);
             Log.i(LOG_TAG, "WORD");
         }
